b976c1ce6cc8ad182f3ae5e6058e628cc45826ee
MakerHuang <huangrunzhe8@outlook.com>
2024-06-07 16:48:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,9 +37,19 @@ print("等待2秒")
 time.sleep(2)
 
 #driver.find_element(By.CSS_SELECTOR,"body > div.main > div.shortcut_area > div:nth-child(2) > div:nth-child(4)")
-driver.find_element(By.XPATH,'//div[2]/div[4]')
 
+driver.switch_to.frame('myIframe')
+driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div[2]/div[4]").click()
+time.sleep(2)
+driver.find_element(By.XPATH,"//*[@id='allState']").click()
+time.sleep(2)
+driver.find_element(By.XPATH,"//*[@id='cs_list']/li[1]/div[1]/img").click()
+time.sleep(2)
+driver.find_element(By.XPATH,"//*[@id='odiv_8834']/div/div/span[2]/table/tbody/tr[4]/td/input").click()
+#driver.find_element(By.XPATH,"")
 
+for i in range(1,5):
+	address = "//*[@id='section7667']/div/h4[" + str(i) + "]/img"
 time.sleep(5)
 #text_input = driver.find_element(By.ID, "用户名")
 #ActionChains(driver).send_keys_to_element(text_input, account)
@@ -57,3 +67,6 @@ for i in range(1,10):
 driver.quit()
 print("Completed!")
 
+//*[@id="section7667"]/div/h4[1]/img
+//*[@id="section7667"]/div/h4[2]/img
+//*[@id="section7674"]/div/h4[4]/img
